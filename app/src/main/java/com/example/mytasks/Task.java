07caae03d544d3267ad9bb285d314b2f6ce700f1
pratikGhatage10 <pratikghatage2424@gmail.com>
2024-08-02 20:24:03 +0530

    Easy food

---
+++ app/src/main/java/com/example/mytasks/Task.java
@@ -0,0 +1,26 @@
+package com.example.mytasks;
+
+public class Task {
+    private String title;
+    private boolean isCompleted;
+    public Task() {
+
+    }
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Task(String title) {
+        this.title = title;
+    }
+}
